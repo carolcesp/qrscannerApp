5ebc7efe804da33457b8391874b21ce993faf7ae
carol cespedes <carolinacespedes90@gmail.com>
2021-11-03 09:22:55 +0100

    tabs creadas, barcode instalado

---
+++ QRScanner/Shared/QRScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  QRScannerApp.swift
+//  Shared
+//
+//  Created by carol cespedes on 3/11/21.
+//
+
+import SwiftUI
+
+@main
+struct QRScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
